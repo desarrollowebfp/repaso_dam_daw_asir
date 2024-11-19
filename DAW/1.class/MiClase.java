e879957ab3139f5f62ea4ed846f9d8c8990bc486
Antonio Rosales <desarrollowebfp@thepower.education>
2024-11-19 16:59:20 +0100

    first class

---
+++ DAW/1.class/MiClase.java
@@ -0,0 +1,16 @@
+/* 
+   public -> modificador de acceso para que sea accesible a lo largo de todo el código
+   class -> palabra reservada para crear una clase (el molde de mis objetos) 
+   
+ */
+
+public class MiClase {
+
+    //public -> modificador acceso otra vez
+    //static -> una instancia o metodo creado directamente en la clase sin necesidad de crear un objeto previamente o recursos adicionales
+    //main -> es el nombre del método reservado que buscara Java para ejecutar por "defecto" -> Java entiende que este es el punto de entrada principal del programa
+    //void -> Void significa que el retorno del método main es "Nada". No confundir 
+    public static void main() {
+      //SIGUIENTE CLASE!
+    }
+}
